b5667cc347d192727bc46eb3b3085bfc14a37a4a
lttrizzle <ltruong1095@gmail.com>
2017-04-21 15:32:46 -0600

    Inserts for database tables done. Logging for parsers and updater completed.

--- Database_API/Xerxes_SQL.py
+++ Database_API/Xerxes_SQL.py
@@ -2,7 +2,8 @@
 import pymysql
 import socket
 import struct
-
+import logging
+import datetime
 # def write_log(log_name, log_info):
 #     with open
 # To map well-known port to the expected service when listing results
@@ -53,12 +54,204 @@ def map_port_service(port_number):
 
 # Local connection to DB, grabbing credentials from local file
 # TODO have Google app connect without needing to call connect
+# TODO encrypt and decrypt file with DB credentials
 def connect_database():
     with open("DatabaseInfo.txt") as f:
         content = f.readlines()
     # you may also want to remove whitespace characters like `\n` at the end of each line
     content = [x.strip() for x in content]
-    return pymysql.connect(content[0], content[1], content[2], content[3], local_infile = 1)
+    try:
+        return pymysql.connect(content[0], content[1], content[2], content[3], local_infile = 1)
+    except:
+        logging.debug("CRITICAL ERROR: CAN'T CONNECT TO DATABASE {}".format(datetime.datetime.now()))
+        exit(-1)
+
+# Basic insert, expects column values to be strings.
+def insert_site_entry(db, IP_address, hostName, ipVersion, region, scanDate):
+    cursor = db.cursor()
+    insertStatement = "INSERT INTO SITE_INFO(IP_ADDRESS, \
+    SITE_NAME, IP_VERSION, COUNTRY, CMS_TYPE, VULNERABILITY_SCORE, CHECKED_DATE) \
+    VALUES ('%s', '%s', '%s', '%s', '%s') \
+    ON DUPLICATE KEY \
+    UPDATE IP_ADDRESS = IP_ADDRESS, SITE_NAME = SITE_NAME, IP_VERSION = IP_VERSION, COUNTRY = COUNTRY, CHECKED_DATE = CHECKED_DATE" % \
+    (IP_address, hostName, ipVersion, region, scanDate)
+
+    try:
+        cursor.execute(insertStatement)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        logging.debug("Failed to insert {} in SITE_INFO Table".format(IP_address))
+        return "[error] with insert entry", e
+
+def update_site_entry(db, IP_address, field, new_value):
+    cursor=db.cursor()
+    update_statement = "UPDATE SITE_INFO SET %s = %s WHERE IP_ADDRESS = %s" % (IP_address, field, new_value)
+    try:
+        cursor.execute(update_statement)
+        db.commit()
+    except:
+        db.rollback()
+        logging.debug("Update for {}, field = {} failed".format(IP_address, field))
+
+def retrieve_site_entry(db, table_field, filter_field, filter_value):
+    cursor = db.cursor()
+    if filter_value == "None":
+        selectStatement = "SELECT %s FROM SITE_INFO" % (table_field)
+    else:
+        selectStatement = "SELECT %s FROM SITE_INFO WHERE %s = %s" % (table_field, filter_field, filter_value)
+    try:
+        cursor.execute(selectStatement)
+        data = cursor.fetchall()
+    except:
+        db.rollback()
+        return  "Site not found"
+    return data
+
+def insert_device_entry(db, IP_address, MAC_address, taxonomy, vendor):
+    cursor = db.cursor()
+    insertStatement = "INSERT INTO DEVICE_INFO \
+    (IP_ADDRESS, MAC_ADDRESS, VENDOR) \
+    VALUES ('%s', '%s', '%s') \
+    ON DUPLICATE KEY \
+    UPDATE IP_ADDRESS = IP_ADDRESS, MAC_ADDRESS = MAC_ADDRESS, VENDOR = VENDOR" % \
+    (IP_address, MAC_address, vendor)
+
+    try:
+        cursor.execute(insertStatement)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        logging.debug("Failed to insert {} in DEVICE_INFO Table".format(IP_address))
+        return "[error] with insert entry", e
+
+def insert_into_whois(db, IP_address, organization):
+    cursor = db.cursor()
+    insertStatement = "INSERT INTO WHOIS_INFO(IP_ADDRESS, ORGANIZATION) \
+    VALUES ('%s', '%s') \
+    ON DUPLICATE KEY \
+    UPDATE IP_ADDRESS = IP_ADDRESS, ORGANIZATION = ORGANIZATION" % \
+    (IP_address, organization)
+
+    try:
+        # Execute the SQL command
+        cursor.execute(insertStatement)
+        db.commit()
+    except:
+        # Rollback in case there is any error
+        db.rollback()
+        logging.debug("Insert failed on WHOIS for IP \"{}\"".format(IP_address))
+
+def insert_into_site_open_services(db, IP_address, portNumber, service_name, banner):
+
+    cursor = db.cursor()
+    insertStatement = "INSERT INTO SITE_OPEN_SERVICES(IP_ADDRESS, PORT_NUMBER, SERVICE_NAME, BANNER) \
+    VALUES ('%s', '%s', '%s', '%s') \
+    ON DUPLICATE KEY \
+    UPDATE IP_ADDRESS = IP_ADDRESS, PORT_NUMBER = PORT_NUMBER, SERVICE_NAME = SERVICE_NAME, BANNER = BANNER " % \
+    (IP_address, int(portNumber), service_name, banner)
+
+    try:
+        # Execute the SQL command
+        cursor.execute(insertStatement)
+        db.commit()
+    except:
+        # Rollback in case there is any error
+        db.rollback()
+        logging.debug("Insert failed on OPEN_SERVICE for IP \"{}\"".format(IP_address))
+
+def insert_into_site_vulnerabilities(db, IP_address, type, description):
+    cursor = db.cursor()
+    # Where type is either a CVE Reference, CMS-related
+    # Description is the CVE ID, or if CMS-related, something such as accessible Admin Login Page
+    insertStatement = "INSERT INTO SITE_VULNERABILITIES (IP_ADDRESS, TYPE, DESCRIPTION) \
+    VALUES ('%s', '%s', '%s') \
+    ON DUPLICATE KEY \
+        UPDATE TYPE = TYPE, DESCRIPTION = DESCRIPTION" % \
+                      (IP_address, type, description)
+
+    try:
+        cursor.execute(insertStatement)
+        db.commit()
+    except:
+        db.rollback()
+        logging.debug("Insert failed for {} site vulnerability: {}".format(IP_address, description))
+
+def retrieve_site_vulnerabilities(db, IP_Address):
+    cursor = db.cursor()
+    try:
+        cursor.execute("SELECT * FROM SITE_VULNERABILITIES WHERE IP_ADDRESS = %s", IP_Address)
+    except:
+        db.rollback()
+        logging.debug("Failed to vulnerabilities for {}".format(IP_Address))
+        return None
+    else:
+        return cursor.fetchall()
+
+def insert_into_scan_history(db, scanID, start_time, end_time):
+    cursor = db.cursor()
+    insertStatement = "INSERT INTO SCAN_HISTORY (SCAN_ID, START_TIME, END_TIME) \
+    VALUES ('%s', '%s', '%s')" % \
+    (int(scanID), start_time, end_time)
+    try:
+        cursor.execute(insertStatement)
+        db.commit()
+    except:
+        logging.debug("Could not record scan history for scan {} from {} to {}".format(scanID, start_time, end_time))
+        db.rollback()
+
+def insert_into_CVE_vulnerabilities(db, CVE_id, status, description):
+    cursor = db.cursor()
+    insertStatement = "INSERT INTO CVE_VULNERABILITIES(CVE_ID, STATUS, DESCRIPTION) \
+    VALUES ('%s', '%s', '%s') \
+    ON DUPLICATE KEY UPDATE  \
+    CVE_ID = VALUES(CVE_ID), STATUS = VALUES(STATUS), DESCRIPTION = VALUES(DESCRIPTION)" % \
+    (CVE_id, status, description)
+
+    try:
+        # Execute the SQL command
+        cursor.execute(insertStatement)
+        # Commit your changes in the database
+        db.commit()
+    except:
+        # Rollback in case there is any error
+        db.rollback()
+        logging.debug("Insert Failed on CVE Update for {}".format(CVE_id))
+
+def insert_scan_requests(db, request_id, requester, target, submission_time):
+    cursor = db.cursor()
+    insertStatement = "INSERT INTO SCAN_REQUESTS (REQUEST_ID, REQUESTER, TARGET_IP, SUBMISSION_TIME) \
+    VALUES ('%s', '%s', '%s', '%s') \
+    ON DUPLICATE IGNORE" % \
+                      (int(request_id), requester, target, submission_time)
+
+    try:
+        # Execute the SQL command
+        cursor.execute(insertStatement)
+        # Commit your changes in the database
+        db.commit()
+    except:
+        # Rollback in case there is any error
+        db.rollback()
+        logging.debug("Failed to record request from {}".format(requester))
+        db.close()
+
+def insert_new_user(db, username, password_hash, salt, level, API_key):
+    cursor = db.cursor()
+    insertStatement = "INSERT INTO USERS (USERNAME, PASSWORD, SALT, LEVEL, API_KEY) \
+    VALUES ('%s', '%s', '%s', '%s', '%s')" % \
+    (username, password_hash, salt, level, API_key)
+
+    try:
+        # Execute the SQL command
+        cursor.execute(insertStatement)
+        # Commit your changes in the database
+        db.commit()
+    except:
+        # Rollback in case there is any error
+        db.rollback()
+        logging.debug("Failed to store account for {}".format(username))
+        db.close()
 
 def get_IP_region(db, IP_address):
     cursor = db.cursor()
@@ -75,9 +268,9 @@ def get_IP_region(db, IP_address):
     except:
         db.rollback()
         return "NULL"
-    data = cursor.fetchall()
-
-    return data[0][0]   # Return the country name serving as the region
+    else:
+        data = cursor.fetchall()
+        return data[0][0]   # Return the country name serving as the region
 
 def get_GEOIP_info(db, IP_address, field):
     cursor = db.cursor()
@@ -86,17 +279,43 @@ def get_GEOIP_info(db, IP_address, field):
     JOIN GEOIP_IP_BLOCKS AS A \
     ON A.GEONAME_ID = B.GEONAME \
     WHERE %s >= A.NETWORK_START AND %s <= A.NETWORK_END" % \
-                      (int_form, int_form)
+    (int_form, int_form)
 
     try:
         cursor.execute(selectStatement)
         db.commit()
     except:
         db.rollback()
+        logging.debug("Error fetching GEOIP Location Data")
         return "NULL"
-    data = cursor.fetchall()
+    else:
+        return cursor.fetchone()
 
-    return data   # Return the country information as a dictionary
+def CMS_extension_lookup(db, name, cms_type):
+    cursor = db.cursor()
+    select_statement = "SELECT EXISTS(SELECT 1  \
+    FROM CMS_VULNERABILITIES \
+    WHERE EXTENSION_NAME = '%s' AND CMS = '%s' \
+    LIMIT 1)" % (name, cms_type)
+
+    try:
+        # Execute the SQL command
+        cursor.execute(select_statement)
+        data = cursor.fetchall()
+        # Commit your changes in the database
+        db.commit()
+    except:
+        # Rollback in case there is any error
+        logging.debug("CMS Extension lookup error")
+        db.rollback()
+        return False
+
+    if data[0] == 1:
+        print("returning true")
+        return True
+    else:
+        return False
+        #db.close()
 
 def retrieve_table_entry(db, tableName, tableField, filterField, filterValue):
     cursor = db.cursor()
@@ -109,206 +328,26 @@ def retrieve_table_entry(db, tableName, tableField, filterField, filterValue):
         return cursor.fetchall()
     except:
         db.rollback()
+        logging.debug("Error fetching {} data".format(tableName))
         return None
 
+# Defunct as update code will just load the data in and themes and plugins have been consolidated into one table
+# def insert_into_vulnerable_plugins(db, name, cms_type, min_ver, max_ver, description):
+#     cursor = db.cursor()
+#     # Where type is either a CVE Reference, CMS-related
+#     # Description is the CVE ID, or if CMS-related, something such as accessible Admin Login Page
+#     insertStatement = "INSERT INTO VULNERABLE_PLUGINS (PLUGIN_NAME, CMS, MIN_VERSION, MAX_VERSION, DESCRIPTION) \
+#     VALUES ('%s', '%s', '%s', '%s', '%s')  \
+#     ON DUPLICATE KEY \
+#         UPDATE CMS = CMS, MIN_VERSION = MIN_VERSION, MAX_VERSION = MAX_VERSION, DESCRIPTION = DESCRIPTION" % \
+#                       (name, cms_type, min_ver, max_ver, description)
+#     try:
+#         cursor.execute(insertStatement)
+#         db.commit()
+#     except:
+#         db.rollback()
 
-# Basic insert, expects column values to be strings.
-def insert_site_entry(db, ipAddress, hostName, ipVersion, region, cms, score, scanDate):
-    cursor = db.cursor()
-    insertStatement = "INSERT INTO SITE_INFO(IP_ADDRESS, \
-    SITE_NAME, IP_VERSION, COUNTRY, CMS_TYPE, VULNERABILITY_SCORE, CHECKED_DATE) \
-    VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s') \
-    ON DUPLICATE KEY \
-    UPDATE IP_ADDRESS = IP_ADDRESS, SITE_NAME = SITE_NAME, IP_VERISON = IP_VERSION, COUNTRY = COUNTRY \
-    CMS_TYPE = CMS_TYPE, VULNERABILITY_SCORE = VULNERABILITY_SCORE, CHECKED_DATE = CHECKED_DATE \ " % \
-    (ipAddress, hostName, ipVersion, region, cms, score, scanDate)
 
-    try:
-        cursor.execute(insertStatement)
-        db.commit()
-    except Exception as e:
-        db.rollback()
-        return "[error] with insert entry", e
-
-def update_site_entry(db, IP_address, field, new_value):
-    cursor=db.cursor()
-    update_statement = "UPDATE SITE_INFO SET %s = %s WHERE IP_ADDRESS = %s" % (IP_address, field, new_value)
-    try:
-        cursor.execute(update_statement)
-        db.commit()
-    except:
-        db.rollback()
-        print("Update failed")
-
-def retrieveSiteEntry(db, tableField, filterField, filterValue):
-    cursor = db.cursor()
-    if filterValue == "None":
-        selectStatement = "SELECT %s FROM SITE_INFO" % (tableField)
-    else:
-        selectStatement = "SELECT %s FROM SITE_INFO WHERE %s = %s" % (tableField, filterField, filterValue)
-    try:
-        cursor.execute(selectStatement)
-        data = cursor.fetchall()
-    except:
-        db.rollback()
-        return  "Site not found"
-    return data
-
-def insert_into_CVE_vulnerabilities(db, CVE_id, status, description):
-
-    cursor = db.cursor()
-    insertStatement = "INSERT INTO CVE_VULNERABILITIES(CVE_ID, STATUS, DESCRIPTION) \
-    VALUES ('%s', '%s', '%s') \
-    ON DUPLICATE KEY UPDATE  \
-    CVE_ID = VALUES(CVE_ID), STATUS = VALUES(STATUS), DESCRIPTION = VALUES(DESCRIPTION)" % \
-    (CVE_id, status, description)
-    # Execute the SQL command
-
-    try:
-        # Execute the SQL command
-        cursor.execute(insertStatement)
-        # Commit your changes in the database
-        db.commit()
-    except:
-        # Rollback in case there is any error
-        db.rollback()
-        print("Insert Failed on CVE Update")
-        #db.close()
-
-def CMS_extension_lookup(db, name, cms_type):
-    cursor = db.cursor()
-    select_statement = "SELECT EXISTS(SELECT 1  \
-    FROM CMS_VULNERABILITIES \
-    WHERE EXTENSION_NAME = '%s' AND CMS = '%s' \
-    LIMIT 1)" % (name, cms_type)
-    cursor.execute(select_statement)
-    data = cursor.fetchone()
-    # try:
-    #     # Execute the SQL command
-    #     cursor.execute(select_statement)
-    #     data = cursor.fetchall()
-    #     # Commit your changes in the database
-    #     db.commit()
-    # except:
-    #     # Rollback in case there is any error
-    #     db.rollback()
-    #     return False
-    print("data = ")
-    print(data[0])
-    if data[0] == 1:
-        print("returning true")
-        return True
-    else:
-        return False
-        #db.close()
-def insert_into_site_open_services(db, siteIP, portNumber, service_name, banner):
-
-    cursor = db.cursor()
-    insertStatement = "INSERT INTO SITE_OPEN_SERVICES(IP_ADDRESS, PORT_NUMBER, SERVICE_NAME, BANNER) \
-    VALUES ('%s', '%s', '%s', '%s') \
-    ON DUPLICATE KEY \
-    UPDATE IP_ADDRESS = IP_ADDRESS, PORT_NUMBER = PORT_NUMBER, SERVICE_NAME = SERVICE_NAME, BANNER = BANNER " % \
-    (siteIP, int(portNumber), service_name, banner)
-
-    cursor.execute(insertStatement)
-    # Commit your changes in the database
-    db.commit()
-    # try:
-    #     # Execute the SQL command
-    #     cursor.execute(insertStatement)
-    #     # Commit your changes in the database
-    #     db.commit()
-    # except:
-    #     # Rollback in case there is any error
-    #     db.rollback()
-    #     print("insert failed on open port")
-    #     #db.close()
-
-def retrieveOpenPortsOnIP(db, siteIP):
-    cursor = db.cursor()
-    data = {}
-    try:
-        cursor.execute("SELECT * FROM SITE_OPEN_PORTS WHERE SITE_IP = %s", siteIP)
-        data = cursor.fetchall()
-    except:
-        db.rollback()
-    for row in data:
-        print(row)
-
-def retrievePlugins(db, plugin_name, CMS_name):
-    cursor = db.cursor()
-    data = {}
-    selectStatement = "SELECT * FROM Vulnerable_Plugins WHERE CMS = %s and PLUGIN_NAME = %s" % (CMS_name, plugin_name)
-    try:
-        cursor.execute(selectStatement)
-        data = cursor.fetchall()
-    except:
-        db.rollback()
-        return "Database Error"
-
-    if not data:
-        return "Plugin not found"
-    else:
-        return data
-
-def insert_into_vulnerable_plugins(db, name, cms_type, min_ver, max_ver, description):
-    cursor = db.cursor()
-    # Where type is either a CVE Reference, CMS-related
-    # Description is the CVE ID, or if CMS-related, something such as accessible Admin Login Page
-    insertStatement = "INSERT INTO VULNERABLE_PLUGINS (PLUGIN_NAME, CMS, MIN_VERSION, MAX_VERSION, DESCRIPTION) \
-    VALUES ('%s', '%s', '%s', '%s', '%s')  \
-    ON DUPLICATE KEY \
-        UPDATE CMS = CMS, MIN_VERSION = MIN_VERSION, MAX_VERSION = MAX_VERSION, DESCRIPTION = DESCRIPTION" % \
-                      (name, cms_type, min_ver, max_ver, description)
-    try:
-        cursor.execute(insertStatement)
-        db.commit()
-    except:
-        db.rollback()
-
-def insert_into_site_vulnerabilities(db, IP_address, type, description):
-    cursor = db.cursor()
-    # Where type is either a CVE Reference, CMS-related
-    # Description is the CVE ID, or if CMS-related, something such as accessible Admin Login Page
-    insertStatement = "INSERT INTO SITE_VULNERABILITIES (IP_ADDRESS, TYPE, DESCRIPTION) \
-    VALUES ('%s', '%s', '%s') \
-    ON DUPLICATE KEY \
-        UPDATE TYPE = TYPE, DESCRIPTION = DESCRIPTION"% \
-                      (IP_address, type, description)
-
-    try:
-        cursor.execute(insertStatement)
-        db.commit()
-    except:
-        db.rollback()
-
-def insertIntoConlusions(db, scanID, vulnerabilityLevel, Result):
-
-    cursor = db.cursor()
-    insertStatement = "INSERT INTO CONCLUSIONS (ID, DESCRIPTION) \
-    VALUES ('%s', '%s')" % \
-                      (int(scanID), Result)
-    try:
-        # Execute the SQL command
-        cursor.execute(insertStatement)
-        # Commit your changes in the database
-        db.commit()
-    except:
-        # Rollback in case there is any error
-        db.rollback()
-        print("insert failed on open port")
-    db.close()
-
-def insertIntoScanHistory(db, scanID, dateTime, scanParameters, conclusionID, sitesVisited):
-    cursor = db.cursor()
-    insertStatement = "INSERT INTO SCAN_HISTORY (SCAN_ID, SCAN_DATETIME, PARAMETERS, CONCLUSION_ID, SITES_SCANNED) \
-    VALUES ('%s', '%s', '%s', '%s', '%s')" % \
-                      (int(scanID), dateTime, scanParameters, conclusionID, sitesVisited)
-    try:
-        cursor.execute(insertStatement)
-        db.commit()
-    except:
-        db.rollback()
 
 
 
--- Parsers/WPScan_Parser.py
+++ Parsers/WPScan_Parser.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python3
 import sys
+import os
+import logging
 sys.path.insert(0,"..\\Database_API")
 import Xerxes_SQL
 
-def parse_plugin_scan_result(IP_Address):
-    db = Xerxes_SQL.connect_database()
-    with open("..\\Test_Documents\\" +IP_Address + "_WPScan.txt", 'r') as infile:
+if __name__ == '__main__':
+    logging.basicConfig(filename="WPScan_Parsing.log", level=logging.DEBUG)
+    file_name = "..\\Test_Documents\\" + sys.argv[1] + "_WPScan.txt"
+    try:
+        infile = open(file_name, 'r')
+    except:
+        logging.debug("Specified WordPress Scan file for {} not found".format(sys.argv[1]))
+    else:
+        db = Xerxes_SQL.connect_database()
         for line in infile:
-            #print(line)
             if line.rstrip('\n') == "[!] The remote website is up, but does not seem to be running WordPress.":
                 db.close()
-                return
+                exit(0)
             if "WordPress version" in line:
                 version_line = line.rsplit()
                 if version_line[3] != "can":
                     Xerxes_SQL.update_site_entry(db, IP_Address, "CMS", "WordPress " + version_line[3])
             if "WordPress theme in use: " in line:
                 theme_line = line.rsplit()
-                print("theme = " + theme_line[5] + " " + theme_line[7])
-                Xerxes_SQL.insert_into_site_vulnerabilities(db, IP_Address, "Theme", theme_line[5] + " " + theme_line[7])
+                Xerxes_SQL.insert_into_site_vulnerabilities(db, IP_Address, "Theme",
+                                                            theme_line[5] + " " + theme_line[7])
             if "plugins found:" in line:
                 for line in infile:
                     if "Name:" in line:
                         plugin_line = line.rsplit()
                         inserted_line = plugin_line[2]
-                        if len(plugin_line) > 3:    # Checks if version was found along with the name
+                        if len(plugin_line) > 3:  # Checks if version was found along with the name
                             inserted_line = inserted_line + " " + plugin_line[4]
-                        print(inserted_line)
                         # Lookup to determine if identified plugin is listed as vulnerable
                         if Xerxes_SQL.CMS_extension_lookup(Xerxes_SQL.connect_database(), plugin_line[2], "WordPress"):
                             Xerxes_SQL.insert_into_site_vulnerabilities(db, IP_Address, "Plugin", inserted_line)
         db.close()
-
-if __name__ == '__main__':
-    parse_plugin_scan_result(sys.argv[1])
+        os.remove(file_name)
 
